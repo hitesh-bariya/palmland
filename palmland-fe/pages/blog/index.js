09cdbea58f637c54b97718176ec652a0e820120e
hitesh.bariya <hiteshbariya310@gmail.com>
2024-09-10 21:26:33 +0530

    resolved blogs issue

--- palmland-fe/pages/blog/index.js
+++ palmland-fe/pages/blog/index.js
@@ -3,19 +3,13 @@ import {
   Button,
   Grid,
   GridItem,
-  Image,
-  SimpleGrid,
   Spinner,
   Text,
 } from "@chakra-ui/react";
-import React, { useEffect } from "react";
-import { useState } from "react";
-import { AtSignIcon, CalendarIcon, ChatIcon } from "@chakra-ui/icons";
+import React, { useEffect, useState } from "react";
 import Link from "next/link";
-import SingleBlog from "@/components/PageComponets/BlogPage/SingleBlog";
-import { BlogData } from "@/common";
-import { getBlogData } from "@/stores/PostProperty/Blog/blogAction";
 import Slider from "react-slick";
+import { getBlogData } from "@/stores/PostProperty/Blog/blogAction";
 
 var settings = {
   dots: false,
@@ -25,13 +19,19 @@ var settings = {
   slidesToScroll: 1,
 };
 
-const blog = () => {
-  const [blogs, setBlogs] = useState();
+const Blog = () => {
+  const [blogs, setBlogs] = useState([]);
   const [loading, setLoading] = useState(true);
+  const [visibleBlogs, setVisibleBlogs] = useState(6); // State for visible blogs
+
   useEffect(() => {
-    // getAllProperty(setProperties, setLoading);
     getBlogData(setBlogs, setLoading);
   }, []);
+
+  const loadMoreBlogs = () => {
+    setVisibleBlogs((prev) => prev + 6); // Increase visible blogs by 6
+  };
+
   if (loading) {
     return (
       <Box
@@ -45,6 +45,7 @@ const blog = () => {
       </Box>
     );
   }
+
   if (!loading && blogs && blogs.length > 0) {
     return (
       <div className="custom__container about__page blog-slider">
@@ -60,69 +61,92 @@ const blog = () => {
           >
             <Text
               variant="p_bold"
-              style={{fontSize: "30px", fontWeight: "700", lineHeight: "50px", color: "#1F343E", marginTop: "50px"}}
+              style={{
+                fontSize: "30px",
+                fontWeight: "700",
+                lineHeight: "50px",
+                color: "#1F343E",
+                marginTop: "50px",
+              }}
               mb={{ base: "10px", lg: "20px" }}
             >
               Featured Blog
             </Text>
             <Slider {...settings}>
-              {blogs.map((blog) => {
-                return(
-                <div>              
-                  <div class="row">
-                    <div class="col-md-6" style={{paddingRight: "30px"}}>
-                      <img src={blog.imageKey}
-                      height="100%" 
-                      width="100%" 
-                      alt="call" 
-                      style={{filter: "brightness(20%)", borderRadius: "7.5px"}}></img>
+              {blogs.slice(0, 3).map((blog) => ( // Display top 3 blogs in slider
+                <div key={blog.id}>
+                  <div className="row">
+                    <div className="col-md-6" style={{ paddingRight: "30px" }}>
+                      <img
+                        src={blog.imageKey}
+                        height="100%"
+                        width="100%"
+                        alt={blog.heading}
+                        style={{
+                          filter: "brightness(20%)",
+                          borderRadius: "7.5px",
+                        }}
+                      />
                     </div>
-                    <div class="col-md-6" style={{paddingLeft: "30px", position:"relative"}}>
+                    <div
+                      className="col-md-6"
+                      style={{ paddingLeft: "30px", position: "relative" }}
+                    >
                       <Text
                         variant="blog_header"
-                        style={{fontSize: "23px", lineHeight: "27px"}}
+                        style={{ fontSize: "23px", lineHeight: "27px" }}
                         mb={{ base: "10px", lg: "20px" }}
                       >
-                        {blog.heading} ({blog.author})
+                        {blog.heading}
                       </Text>
                       <Text
                         variant="blog_text"
-                        style={{fontSize: "15px", lineHeight: "24px"}}
+                        style={{ fontSize: "15px", lineHeight: "24px" }}
                         mb={{ base: "10px", lg: "20px" }}
                       >
-                          {blog.content}
+                        {blog.content}
                       </Text>
-                      <div style={{position: "absolute", bottom: 0}}>
+                      <div style={{ position: "absolute", bottom: 0 }}>
                         <Text
                           variant="newsletter_publish"
-                          style={{fontSize: "13px", lineHeight: "28px"}}
+                          style={{ fontSize: "13px", lineHeight: "28px" }}
                           mb={{ base: "10px", lg: "20px" }}
                         >
-                          <span style={{display: "flex", lineHeight: "20px", color: "#6D6D6D"}}>
-                            <img src="/assets/icons/vector.png" height="3px" width="18px" alt="call" style={{paddingBottom: "10px"}}></img>
-                            &nbsp;&nbsp;&nbsp;&nbsp;Published on {blog.postedDate}  &nbsp;&nbsp;&nbsp;
-                            <img src="/assets/icons/publish-date.png" height="3px" width="18px" alt="call" style={{paddingBottom: "10px"}}></img>
-                            &nbsp;&nbsp;&nbsp;&nbsp;Published on {blog.publishedDate}
+                          <span
+                            style={{
+                              display: "flex",
+                              lineHeight: "20px",
+                              color: "#6D6D6D",
+                            }}
+                          >
+                            Published on {blog.postedDate}
                           </span>
                         </Text>
                         <Link href={`/blog/${blog.id}`} as={`/blog/${blog.id}`}>
-                          <Button variant="secondary" size="14" style={{color: "#EAB258"}}>
+                          <Button
+                            variant="secondary"
+                            size="14"
+                            style={{ color: "#EAB258" }}
+                          >
                             Read More
                           </Button>
                         </Link>
                       </div>
                     </div>
                   </div>
-                </div>)
-              })}
+                </div>
+              ))}
             </Slider>
-            {/* {blogs.map((blog) => {
-              return <SingleBlog key={blog.id} blog={blog} />;
-            })} */}
 
             <Text
               variant="p_bold"
-              style={{fontSize: "30px", fontWeight: "700", lineHeight: "50px", color: "#1F343E", marginTop: "50px"}}
+              style={{
+                fontSize: "30px",
+                fontWeight: "700",
+                lineHeight: "50px",
+                color: "#1F343E",
+                marginTop: "50px",
+              }}
               mb={{ base: "10px", lg: "20px" }}
             >
               All Resources
@@ -133,146 +157,80 @@ const blog = () => {
               alignItems="flex-end"
               paddingTop={"30px"}
             >
-              <GridItem colSpan={{ base: 5, md: 1 }}>
-                <Box
-                  w={"100%"}
-                  className="banner__field__block"
-                >
-                  <div class="card" style={{border: "2px solid #EAB258", borderRadius: "25px", padding: "10px"}}>
-                    <img src="/assets/images/blog1.png" alt="call" style={{paddingBottom: "30px", width: "-webkit-fill-available"}}></img>
-                    <div style={{padding: "0 10px"}}>
-                      <p style={{fontSize: "15px", lineHeight: "20.3px", fontWeight: "700"}}>Lorem ipsum dolor sit amet, consec tetur adipiscing</p>
-                      <p>Discover Engrace Vista, an exclusive 3 BHK community off Sarjapur Road, Bangalore. Experience the best of urban living with spacious apartments, top-tier amenities, enhanced security, and a vibrant community atmosphere</p>
-                      <img src="/assets/icons/hr-golden.png" height="3px" width="18px" alt="call" style={{paddingBottom: "10px"}}></img>
-                      <Text
-                        variant="s_regular"
-                        style={{fontSize: "9px", lineHeight: "14.06px", color: "#000", width: "100%", marginBottom: "20px"}}
-                      >
-                        <span style={{float: "left"}}>May,2024</span>
-                        <span style={{float: "right"}}>8 min read</span>
-                      </Text>     
-                    </div>   
-                  </div>
-                </Box>
-              </GridItem>
-              <GridItem colSpan={{ base: 5, md: 1 }}>
-                <Box
-                  w={"100%"}
-                  className="banner__field__block"
-                >
-                  <div class="card" style={{border: "2px solid #EAB258", borderRadius: "25px", padding: "10px"}}>
-                    <img src="/assets/images/blog2.png" alt="call" style={{paddingBottom: "30px", width: "-webkit-fill-available"}}></img>
-                    <div style={{padding: "0 10px"}}>
-                      <p style={{fontSize: "15px", lineHeight: "20.3px", fontWeight: "700"}}>Lorem ipsum dolor sit amet, consec tetur adipiscing</p>
-                      <p>Discover Engrace Vista, an exclusive 3 BHK community off Sarjapur Road, Bangalore. Experience the best of urban living with spacious apartments, top-tier amenities, enhanced security, and a vibrant community atmosphere</p>
-                      <img src="/assets/icons/hr-golden.png" height="3px" width="18px" alt="call" style={{paddingBottom: "10px"}}></img>
-                      <Text
-                        variant="s_regular"
-                        style={{fontSize: "9px", lineHeight: "14.06px", color: "#000", width: "100%", marginBottom: "20px"}}
-                      >
-                        <span style={{float: "left"}}>May,2024</span>
-                        <span style={{float: "right"}}>8 min read</span>
-                      </Text>     
-                    </div>   
-                  </div>
-                </Box>
-              </GridItem>
-              <GridItem colSpan={{ base: 5, md: 1 }}>
-                <Box
-                  w={"100%"}
-                  className="banner__field__block"
-                >
-                  <div class="card" style={{border: "2px solid #EAB258", borderRadius: "25px", padding: "10px"}}>
-                    <img src="/assets/images/blog3.png" alt="call" style={{paddingBottom: "30px", width: "-webkit-fill-available"}}></img>
-                    <div style={{padding: "0 10px"}}>
-                      <p style={{fontSize: "15px", lineHeight: "20.3px", fontWeight: "700"}}>Lorem ipsum dolor sit amet, consec tetur adipiscing</p>
-                      <p>Discover Engrace Vista, an exclusive 3 BHK community off Sarjapur Road, Bangalore. Experience the best of urban living with spacious apartments, top-tier amenities, enhanced security, and a vibrant community atmosphere</p>
-                      <img src="/assets/icons/hr-golden.png" height="3px" width="18px" alt="call" style={{paddingBottom: "10px"}}></img>
-                      <Text
-                        variant="s_regular"
-                        style={{fontSize: "9px", lineHeight: "14.06px", color: "#000", width: "100%", marginBottom: "20px"}}
-                      >
-                        <span style={{float: "left"}}>May,2024</span>
-                        <span style={{float: "right"}}>8 min read</span>
-                      </Text>     
-                    </div>   
-                  </div>
-                </Box>
-              </GridItem>
-              <GridItem colSpan={{ base: 5, md: 1 }}>
-                <Box
-                  w={"100%"}
-                  className="banner__field__block"
-                >
-                  <div class="card" style={{border: "2px solid #EAB258", borderRadius: "25px", padding: "10px"}}>
-                    <img src="/assets/images/blog4.png" alt="call" style={{paddingBottom: "30px", width: "-webkit-fill-available"}}></img>
-                    <div style={{padding: "0 10px"}}>
-                      <p style={{fontSize: "15px", lineHeight: "20.3px", fontWeight: "700"}}>Lorem ipsum dolor sit amet, consec tetur adipiscing</p>
-                      <p>Discover Engrace Vista, an exclusive 3 BHK community off Sarjapur Road, Bangalore. Experience the best of urban living with spacious apartments, top-tier amenities, enhanced security, and a vibrant community atmosphere</p>
-                      <img src="/assets/icons/hr-golden.png" height="3px" width="18px" alt="call" style={{paddingBottom: "10px"}}></img>
-                      <Text
-                        variant="s_regular"
-                        style={{fontSize: "9px", lineHeight: "14.06px", color: "#000", width: "100%", marginBottom: "20px"}}
-                      >
-                        <span style={{float: "left"}}>May,2024</span>
-                        <span style={{float: "right"}}>8 min read</span>
-                      </Text>     
-                    </div>   
-                  </div>
-                </Box>
-              </GridItem>
-              <GridItem colSpan={{ base: 5, md: 1 }}>
-                <Box
-                  w={"100%"}
-                  className="banner__field__block"
-                >
-                  <div class="card" style={{border: "2px solid #EAB258", borderRadius: "25px", padding: "10px"}}>
-                    <img src="/assets/images/blog5.png" alt="call" style={{paddingBottom: "30px", width: "-webkit-fill-available"}}></img>
-                    <div style={{padding: "0 10px"}}>
-                      <p style={{fontSize: "15px", lineHeight: "20.3px", fontWeight: "700"}}>Lorem ipsum dolor sit amet, consec tetur adipiscing</p>
-                      <p>Discover Engrace Vista, an exclusive 3 BHK community off Sarjapur Road, Bangalore. Experience the best of urban living with spacious apartments, top-tier amenities, enhanced security, and a vibrant community atmosphere</p>
-                      <img src="/assets/icons/hr-golden.png" height="3px" width="18px" alt="call" style={{paddingBottom: "10px"}}></img>
-                      <Text
-                        variant="s_regular"
-                        style={{fontSize: "9px", lineHeight: "14.06px", color: "#000", width: "100%", marginBottom: "20px"}}
-                      >
-                        <span style={{float: "left"}}>May,2024</span>
-                        <span style={{float: "right"}}>8 min read</span>
-                      </Text>     
-                    </div>   
-                  </div>
-                </Box>
-              </GridItem>
-              <GridItem colSpan={{ base: 5, md: 1 }}>
-                <Box
-                  w={"100%"}
-                  className="banner__field__block"
-                >
-                  <div class="card" style={{border: "2px solid #EAB258", borderRadius: "25px", padding: "10px"}}>
-                    <img src="/assets/images/blog6.png" alt="call" style={{paddingBottom: "30px", width: "-webkit-fill-available"}}></img>
-                    <div style={{padding: "0 10px"}}>
-                      <p style={{fontSize: "15px", lineHeight: "20.3px", fontWeight: "700"}}>Lorem ipsum dolor sit amet, consec tetur adipiscing</p>
-                      <p>Discover Engrace Vista, an exclusive 3 BHK community off Sarjapur Road, Bangalore. Experience the best of urban living with spacious apartments, top-tier amenities, enhanced security, and a vibrant community atmosphere</p>
-                      <img src="/assets/icons/hr-golden.png" height="3px" width="18px" alt="call" style={{paddingBottom: "10px"}}></img>
-                      <Text
-                        variant="s_regular"
-                        style={{fontSize: "9px", lineHeight: "14.06px", color: "#000", width: "100%", marginBottom: "20px"}}
-                      >
-                        <span style={{float: "left"}}>May,2024</span>
-                        <span style={{float: "right"}}>8 min read</span>
-                      </Text>     
-                    </div>   
-                  </div>
-                </Box>
-              </GridItem>
+              {blogs.slice(0, visibleBlogs).map((blog) => (
+                <GridItem colSpan={{ base: 5, md: 1 }} key={blog.id}>
+                  <Box w={"100%"} className="banner__field__block">
+                    <div
+                      className="card"
+                      style={{
+                        border: "2px solid #EAB258",
+                        borderRadius: "25px",
+                        padding: "10px",
+                      }}
+                    >
+                      <img
+                        src={blog.imageKey}
+                        alt={blog.heading}
+                        style={{ paddingBottom: "30px", width: "100%" }}
+                      />
+                      <div style={{ padding: "0 10px" }}>
+                        <p
+                          style={{
+                            fontSize: "15px",
+                            lineHeight: "20.3px",
+                            fontWeight: "700",
+                          }}
+                        >
+                          {blog.heading}
+                        </p>
+                        <p>{blog.content}</p>
+                        <img
+                          src="/assets/icons/hr-golden.png"
+                          height="3px"
+                          width="18px"
+                          alt="separator"
+                          style={{ paddingBottom: "10px" }}
+                        />
+                        <Text
+                          variant="s_regular"
+                          style={{
+                            fontSize: "9px",
+                            lineHeight: "14.06px",
+                            color: "#000",
+                            width: "100%",
+                            marginBottom: "20px",
+                          }}
+                        >
+                          <span style={{ float: "left" }}>{blog.date}</span>
+                          <span style={{ float: "right" }}>
+                            {blog.readTime} min read
+                          </span>
+                        </Text>
+                      </div>
+                    </div>
+                  </Box>
+                </GridItem>
+              ))}
             </Grid>
-            <div style={{width: "-webkit-fill-available",  alignItems: "center", textAlign: "center", padding: "60px 0 30px 0"}}>
-              <Link href={`/blog/${blog.id}`} as={`/blog/${blog.id}`}>
-                <Button variant="secondary" size="14" style={{color: "#EAB258"}}>
+
+            {visibleBlogs < blogs.length && (
+              <div
+                style={{
+                  width: "100%",
+                  textAlign: "center",
+                  padding: "60px 0 30px 0",
+                }}
+              >
+                <Button
+                  onClick={loadMoreBlogs}
+                  variant="secondary"
+                  size="14"
+                  style={{ color: "#EAB258" }}
+                >
                   Load More
-                </Button>  
-              </Link>
-            </div>
+                </Button>
+              </div>
+            )}
           </Box>
         </div>
       </div>
@@ -280,4 +238,4 @@ const blog = () => {
   }
 };
 
-export default blog;
+export default Blog;
